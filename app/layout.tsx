a8c0be2e5de3733ebeef0178c90376a4c907e9ab
ezekiel <ezekielforko875@gmail.com>
2025-06-08 11:16:43 +0000

    make the favi work

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "RoosBoard",
   description: "your Online dashboard",
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
         ignoreBuildErrors: true,
     },
     output: 'standalone',
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(ico|png|jpg|jpeg|gif)$/,
+            type: 'asset/resource'
+        });
+        return config;
+    },
     experimental: {
         serverActions: true,
     },
